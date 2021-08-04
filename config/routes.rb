90674f9a2517330a50c556c9f0a0f1b19c4f97cf
Kevin Jo <kevinhjo725@gmail.com>
2021-08-03 23:44:34 -0400

    Create post creation branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :destroy]
     resource :session, only: [:create, :destroy]
+    resources :posts, only: [:index, :create]
   end
 end
