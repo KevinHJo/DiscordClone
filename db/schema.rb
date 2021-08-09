04845f4951777c90af2b98b79506cbd2d8f2128e
Kevin Jo <kevinhjo725@gmail.com>
2021-08-09 13:36:36 -0400

    Migrate channel

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_172128) do
+ActiveRecord::Schema.define(version: 2021_08_09_172956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "server_id", null: false
+    t.boolean "private", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id"], name: "index_channels_on_server_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "body", null: false
