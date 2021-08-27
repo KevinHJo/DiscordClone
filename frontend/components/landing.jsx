9055fef03f57d3065c7c7246f5baa3495e71c75b
Kevin Jo <kevinhjo725@gmail.com>
2021-08-27 14:03:38 -0400

    Add background images to landing page banner

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
   render() {
-    return null
+    return (
+      <div className='landing-page'>
+        <div className='banner-container'>
+          <span className='landing-page-banner'>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </div>
+    )
   }
 }
 
