cee242c73e4f7425aef36feba4225d582cd37dfb
Kevin Jo <kevinhjo725@gmail.com>
2021-08-27 18:08:10 -0400

    Create basic landing page

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    console.log('hello');
+  }
+
   render() {
     return (
       <div className='landing-page'>
         <div className='landing-page-banner'>
           <div className='landing-page-nav'>
-            <img src="https://github.com/KevinHJo/DiscordClone/blob/main/app/assets/images/discord-logo.png?raw=true" />
-            <Link to="/login">Login</Link>
+            <Link to='/' className='landing-home-link'>
+              <img src="https://github.com/KevinHJo/DiscordClone/blob/main/app/assets/images/discord-logo.png?raw=true" onClick={this.onClick}/>
+            </Link>
+            <Link to="/login" className='landing-login'>Login</Link>
           </div>
 
           <div className='banner-message'>
@@ -23,6 +36,18 @@ class LandingPage extends React.Component {
             <img src="https://raw.githubusercontent.com/KevinHJo/DiscordClone/9055fef03f57d3065c7c7246f5baa3495e71c75b/app/assets/images/banner-element-2.svg" className='banner-element-2' />
             <img src="https://raw.githubusercontent.com/KevinHJo/DiscordClone/9055fef03f57d3065c7c7246f5baa3495e71c75b/app/assets/images/banner-element-1.svg" className='banner-element-1' />
           </div>
+        </div>
+
+        <div className='work-in-progress-1'>
+          <h1 className='wip-message'>More Panels Coming! :)</h1>
+        </div>
+
+        <div className='work-in-progress-2'>
+          <h1 className='wip-message'>More Panels Coming! :)</h1>
+        </div>
+        
+        <div className='work-in-progress-3'>
+          <h1 className='wip-message'>More Panels Coming! :)</h1>
         </div>
       </div>
     )
