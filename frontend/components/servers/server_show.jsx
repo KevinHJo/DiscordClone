a3a16fb3329613004f42a327a655aecb1b17a0b9
Kevin Jo <kevinhjo725@gmail.com>
2021-10-08 09:54:49 -0400

    Prepare for merge

--- frontend/components/servers/server_show.jsx
+++ frontend/components/servers/server_show.jsx
@@ -20,8 +20,7 @@ class ServerShow extends React.Component {
     }
 
     componentDidMount() {
-        // this.props.fetchUserServers(this.props.currentUser.id);
-        // this.props.fetchUserServers(this.props.currentUser.id);
+        this.props.fetchUserServers(this.props.currentUser.id);
         this.props.fetchChannel(this.props.channelId);
         this.setState({channel: this.props.channel})
     }
@@ -114,8 +113,8 @@ class ServerShow extends React.Component {
                             <p className='user-panel-username'>{this.props.currentUser.username}</p>
 
                             <nav className='user-settings-nav'>
-                                {/* <i className="fas fa-cog" onClick={this.displayUserSettings}></i> */}
-                                <div className="fas fa-cog" onClick={this.displayUserSettings}></div> {/* REMOVE AFTER TESTING */}
+                                <i className="fas fa-cog" onClick={this.displayUserSettings}></i>
+                                {/* <div className="fas fa-cog" onClick={this.displayUserSettings}></div> */}
                             </nav>
                         </div>
                     </div>
